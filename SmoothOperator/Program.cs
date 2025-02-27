1da7a8a1c96d6f4b0d88652955e708ba409a2632
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-27 21:18:54 +0000

    Increment and decrement project.

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro:");
+            string num = Console.ReadLine();
+            sbyte n = Convert.ToSByte(num);
+            n = --n;
+            Console.WriteLine($"{n}");
+            n = ++n;
+            Console.WriteLine($"{n}");
+            
+            
+
+        }
+    }
+}
