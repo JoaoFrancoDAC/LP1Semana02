de76c5123d321e0b2f2ce51cbc5368c41e28c5c4
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-27 21:53:43 +0000

    Added the resolution of the project

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -14,8 +14,6 @@ namespace SmoothOperator
             n = ++n;
             Console.WriteLine($"{n}");
             
-            
-
         }
     }
 }
--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,21 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine($"{byte.MinValue}");
+            Console.WriteLine($"{byte.MaxValue}");
+            Console.WriteLine($"{sbyte.MinValue}");
+            Console.WriteLine($"{sbyte.MaxValue}");
+            Console.WriteLine($"{short.MinValue}");
+            Console.WriteLine($"{short.MaxValue}");
+            Console.WriteLine($"{int.MinValue}");
+            Console.WriteLine($"{int.MaxValue}");
+            Console.WriteLine($"{long.MinValue}");
+            Console.WriteLine($"{long.MaxValue}");
+            Console.WriteLine($"{float.MinValue}");
+            Console.WriteLine($"{float.MaxValue}");
+            Console.WriteLine($"{double.MinValue}");
+            Console.WriteLine($"{double.MaxValue}");
+            
         }
     }
 }
