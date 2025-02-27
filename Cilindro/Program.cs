81a5692767b39b2625f83c06e577f450183795ee
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-27 21:44:52 +0000

    Added the resolution of the problem

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+
 namespace Cilindro
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Qual altura do cilindro?");
+            String alturaString = Console.ReadLine();
+            double altura = Convert.ToDouble(alturaString);
+
+            Console.WriteLine("Qual raio do cilindro?");
+            String raioString = Console.ReadLine();
+            double raio = Convert.ToDouble(raioString);
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            double areaSuperficie = 2 * Math.PI * raio * altura;
+            Console.WriteLine($"Volume do cilindro: {volume}");
+            Console.WriteLine($"Area de superfície do cilindro: {areaSuperficie}");
+
+
+
         }
     }
 }
