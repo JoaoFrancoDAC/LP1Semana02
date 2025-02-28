05feaee962aa9e0bcff919fd5b9f9d980ad2a928
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-28 00:22:15 +0000

    Removido acentos pois o programa não reconhecia a semelhança entre as strings com eles

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Formats.Asn1;
 
 namespace ChatGPTOne
 {
@@ -6,7 +7,41 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            bool Answer = false;
+            while (Answer==false)
+            {
+                Console.WriteLine("Insira a questão ou digite EXIT para terminar:");
+                string userQuestion = Console.ReadLine();
+                if (userQuestion == "EXIT")
+                {
+                    Answer = true;
+                }
+                else if ( userQuestion == "Oi, como saio deste looping?")
+                {
+                    Console.WriteLine("Pode deixar que eu fecho para você.");
+                    Answer = true;
+                }
+                else if (userQuestion == "Qual e o seu nome?")
+                {
+                    Console.WriteLine("Meu nome é ChatGPTOne. Adeus.");
+                    Answer = true;
+                }
+                else if (userQuestion == "Qual e a sua idade?")
+                {
+                    Console.WriteLine("Olha, eu acabei de ser escrito, entao provavelmente tenho menos de 1 mes...");
+                    Answer = true;
+                }
+                else if (userQuestion == "Sera que eu vou passar nesta cadeira?")
+                {
+                    Console.WriteLine("Olha rapaz... boa sorte");
+                    Answer = true;
+                }
+                else 
+                {
+                    Console.WriteLine("Desculpe, não sei responder a esta questão.");
+                    Answer = false;
+                }
+            }
         }
     }
 }
